1122ba937912df888c1c058838a7aed8255a8e02
proider <162008145+proider@users.noreply.github.com>
2024-04-22 20:54:45 +0800

    Add files via upload

---
+++ practise_34.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) {
+	
+	int array[] = { 2,9,5,4,6,7,2,3,4,1,8,0 };
+	for (size_t i = 1; i < 12; ++i) {
+		int key = array[i];
+		int j = i - 1;
+		while (j >= 0 && array[j] >= key) {
+			array[j + 1] = array[j];
+			j = j - 1;
+		}
+		array[j + 1] = key;
+	}
+	for (size_t i = 0; i < 12; ++i) {
+
+		printf("%d  ", array[i]);
+	}
+
+
+	return 0;
+}
