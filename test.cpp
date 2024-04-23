3bc4003ca76c3a6b0b978c38f2f8b3396ba096ce
proider <162008145+proider@users.noreply.github.com>
2024-04-23 19:47:51 +0800

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -1,43 +1,22 @@
-//2024.4.21
-//luogu P1085
 #include <stdio.h>
-
-typedef struct {
-	int study_time1;
-	int study_time2;
-	char day;
-	int reflect;
-}Total_time;
 int main(void) {
-	int complete_happy = 0;
-	int found = 1;
-	Total_time unhappy_time[7] = { 0 };
-	Total_time one_week = { 0 };
-	int max_unhappy_day = 0;
-	for (int i = 0; i < 7; ++i) {
-		scanf_s("%d %d",&one_week.study_time1,&one_week.study_time2);
-		if (one_week.study_time1 + one_week.study_time2 > 8) {
-			unhappy_time[i].study_time1 = one_week.study_time1;
-			unhappy_time[i].study_time2 = one_week.study_time2;
-			unhappy_time[i].day = i+1;
-			unhappy_time[i].reflect = 0;
-			complete_happy++;
-		}
-	}
-	if (complete_happy == 0) {
-		printf("0");
-		return 0;
-	}
-	for (int i = 0; i < 7; ++i) {
-		for (int j = 0; j < 7; ++j) {
-			if (unhappy_time[i].study_time1 + unhappy_time[i].study_time2 >= unhappy_time[j].study_time1 + unhappy_time[j].study_time2) {
-				unhappy_time[i].reflect++;
-			}
-		}
-		if (unhappy_time[i].reflect == 7) {
-			printf("%d", unhappy_time[i].day);
+	int cigarette_number = 0;
+	int agiotage_number = 0;
+	int total_cigarette = 0;
+	int count = 0;
+		scanf_s("%d %d", &cigarette_number, &agiotage_number);
+		
+		if (cigarette_number < agiotage_number||agiotage_number==1|| agiotage_number==0) {
+			printf("%d", total_cigarette);
 			return 0;
 		}
-	}
+		while (cigarette_number >= agiotage_number) {
+			total_cigarette+= agiotage_number;
+			cigarette_number = cigarette_number-agiotage_number+1;
+			count++;
+		}
+		total_cigarette += cigarette_number;
+
+		printf("%d", total_cigarette);
 	return 0;
 }
